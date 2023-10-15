9ce62e13b3dfbf6333ec1ce416ef5912afa31c06
balasubramanian <bbalax.bbalax@gmail.com>
2023-10-15 20:53:16 +0530

    Delete mockTrello directory

--- mockTrello/src/components/index.js
+++
@@ -1,9 +0,0 @@
-import AddOne from './addOne';
-import List from './list';
-import NavBar from './navBar';
-import { mockData } from './mock-data'
-import EditCardModal from './editCardModal';;
-
-export {
-    AddOne, List, NavBar, mockData,EditCardModal
-}
